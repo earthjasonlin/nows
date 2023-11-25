839f79eca20dae7da51338ec848162cf2ade4581
Zichao Lin <earthjasonlin@163.com>
2023-11-25 20:18:34 +0800

    update api

--- api.php
+++ api.php
@@ -8,4 +8,13 @@ $sql = "SELECT * FROM soul ORDER BY RAND() LIMIT 1";
 $result = mysqli_query($conn, $sql);
 $row = mysqli_fetch_assoc($result);
 //输出json
-echo json_encode(array('code' => 1, 'data' => $row['title']));
+echo json_encode(array(
+    "code" => 0,
+    "message" => "OK",
+    "data" => array(
+        "id" => $row["id"],
+        "title" => $row["title"],
+        "author" => $row["author"],
+        "from" => $row["from"]
+    )
+));
